77e59e96bae9c57698a54d277d79266cd347c791
kidcompassion <sallypoulsen@gmail.com>
2016-01-17 14:53:07 -0700

    gets mobile media query working and add click handling to mobile nav.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+ 
   $('.mobile-toggle').click(function(){
     if ($('.primary > ul').is(':hidden')){
       $('.primary > ul').show();
     }else{
       $('.primary > ul').hide();
+    }
+  });
+
+  $('.primary ul li').click(function(){
+    console.log($(this).find('>ul'));
+    if ($(this).find('> ul').is(':hidden')){
+      $(this).find('> ul').show();
+    }else {
+      $(this).find('> ul').hide();
     }
   });
 
